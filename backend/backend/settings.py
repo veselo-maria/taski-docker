e22d7f0a14318087a6e4fdf54b64f30f79c3ea85
Maria Veselovskaya <mariaveselovskaa64258@gmail.com>
2026-07-19 21:49:23 +0300

    change settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '81.26.183.245', 'plannerx.hopto.org']
 
 # Application definition
 
